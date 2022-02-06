f220aa6547a12c2c649841b64bc9d47ed92ff4ca
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2022-02-06 11:44:01 -0600

    created ~/.config/i3/polybar.sh
    Don't ask why it is in my i3 folder when all the wikis tell you to put it in the polybar folder I just felt different

---
+++ .config/i3/polybar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# launch the bar
+polybar topbar
