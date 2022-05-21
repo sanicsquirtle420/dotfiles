24db91f894812f7a304684c52c158a106d252053
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2022-05-21 13:35:35 -0500

    Update dmenu-select.sh

--- .scripts/dmenu-select.sh
+++ .scripts/dmenu-select.sh
@@ -8,6 +8,7 @@ then
 	cp -r $HOME/Documents/i3-themes/aqua/i3/config $HOME/.config/i3/config &&
         cp -r $HOME/Documents/i3-themes/aqua/polybar/config.ini $HOME/.config/polybar/config.ini &&
 	cp -r $HOME/Documents/i3-themes/aqua/rofi/config.rasi $HOME/.config/rofi/config.rasi &&
+	nitrogen --set-scaled --random $HOME/.wallpapers/Aqua/
 	i3-msg restart
 
 elif [ $THEME == 'Yotsuba' ]
@@ -16,6 +17,7 @@ then
 	cp -r $HOME/Documents/i3-themes/yotsuba/i3/config $HOME/.config/i3/config &&
         cp -r $HOME/Documents/i3-themes/yotsuba/polybar/config.ini $HOME/.config/polybar/config.ini &&
         cp -r $HOME/Documents/i3-themes/yotsuba/rofi/config.rasi $HOME/.config/rofi/config.rasi &&
+	nitrogen --set-scaled --random $HOME/.wallpapers/Yotsuba/
 	i3-msg restart
 
 elif [ $THEME == 'Mai' ]
@@ -24,7 +26,8 @@ then
         cp -r $HOME/Documents/i3-themes/mai/i3/config $HOME/.config/i3/config &&
         cp -r $HOME/Documents/i3-themes/mai/polybar/config.ini $HOME/.config/polybar/config.ini &&
         cp -r $HOME/Documents/i3-themes/mai/rofi/config.rasi $HOME/.config/rofi/config.rasi &&
-        i3-msg restart
+        nitrogen --set-scaled --random $HOME/.wallpapers/Mai/
+	i3-msg restart
 
 elif [ $THEME == 'EXIT' ]
 then
