6b93291d9bdf7503dbd5b3e18142f82756f5569b
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2022-10-08 22:32:13 -0500

    ! ! !

--- .scripts/dmenu-select.sh
+++ .scripts/dmenu-select.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
+#!/bin/bash
+
 THEME=$(echo -e "Aqua\nYotsuba\nMai\nEXIT" | dmenu -i -p "Which theme do you want to switch to?")
 
 if [ $THEME == 'Aqua' ]
 then
 	notify-send "You selected the $THEME theme. All config files have been updated." &&
-	cp -r $HOME/Documents/i3-themes/aqua/i3/config $HOME/.config/i3/config &&
-        cp -r $HOME/Documents/i3-themes/aqua/polybar/config.ini $HOME/.config/polybar/config.ini &&
-	cp -r $HOME/Documents/i3-themes/aqua/rofi/config.rasi $HOME/.config/rofi/config.rasi &&
-	nitrogen --set-scaled --random $HOME/.wallpapers/Aqua/
+	cp -r $HOME/i3-themes/aqua/i3/config $HOME/.config/i3/config &&
+        
+	cp -r $HOME/i3-themes/aqua/rofi/launchers/type-2/shared/colors.rasi $HOME/.config/rofi/launchers/type-2/shared/colors.rasi &&
+	cp -r $HOME/i3-themes/aqua/polybar/colors.ini $HOME/.config/polybar/colors.ini
 	i3-msg restart
 
 elif [ $THEME == 'Yotsuba' ]
 then
 	notify-send "You selected the $THEME theme. All config files have been updated." &&
-	cp -r $HOME/Documents/i3-themes/yotsuba/i3/config $HOME/.config/i3/config &&
-        cp -r $HOME/Documents/i3-themes/yotsuba/polybar/config.ini $HOME/.config/polybar/config.ini &&
-        cp -r $HOME/Documents/i3-themes/yotsuba/rofi/config.rasi $HOME/.config/rofi/config.rasi &&
-	nitrogen --set-scaled --random $HOME/.wallpapers/Yotsuba/
+	cp -r $HOME/i3-themes/yotsuba/i3/config $HOME/.config/i3/config &&
+         
+        cp -r $HOME/i3-themes/yotsuba/rofi/launchers/type-2/shared/colors.rasi $HOME/.config/rofi/launchers/type-2/shared/colors.rasi &&
+	cp -r $HOME/i3-themes/yotsuba/polybar/colors.ini $HOME/.config/polybar/colors.ini
 	i3-msg restart
 
 elif [ $THEME == 'Mai' ]
 then
         notify-send "You selected the $THEME theme. All config files have been updated." &&
-        cp -r $HOME/Documents/i3-themes/mai/i3/config $HOME/.config/i3/config &&
-        cp -r $HOME/Documents/i3-themes/mai/polybar/config.ini $HOME/.config/polybar/config.ini &&
-        cp -r $HOME/Documents/i3-themes/mai/rofi/config.rasi $HOME/.config/rofi/config.rasi &&
-        nitrogen --set-scaled --random $HOME/.wallpapers/Mai/
+        cp -r $HOME/i3-themes/mai/i3/config $HOME/.config/i3/config &&
+         
+        cp -r $HOME/i3-themes/mai/rofi/launchers/type-2/shared/colors.rasi $HOME/.config/rofi/launchers/type-2/shared/colors.rasi &&
+        cp -r $HOME/i3-themes/mai/polybar/colors.ini $HOME/.config/polybar/colors.ini
 	i3-msg restart
 
 elif [ $THEME == 'EXIT' ]
