781bbb79ea81bc89dd81ebb2951e121b18c005ac
Diego <94948940+sanicsquirtle420@users.noreply.github.com>
2024-05-12 22:08:10 -0500

    Update change-to-aqua.sh

--- .scripts/change-to-aqua.sh
+++ .scripts/change-to-aqua.sh
@@ -4,6 +4,6 @@ THEME="Aqua"
 
 notify-send "You selected the $THEME theme. All config files have been updated." &&
 	cp -r $HOME/.themes/$THEME/i3/colors.conf $HOME/.config/i3/colors.conf &&
-	cp -r $HOME/.themes/$THEME/rofi/colors.rasi $HOME/.config/rofi/colors.rasi &&
+	cp -r $HOME/.themes/$THEME/rofi/colors.rasi $HOME/.config/rofi/shared/colors.rasi &&
 	cp -r $HOME/.themes/$THEME/polybar/colors.ini $HOME/.config/polybar/colors.ini &&
 	i3-msg-restart
