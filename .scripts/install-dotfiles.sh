ac0be09c17386c9ef61d61c46794a3367c1ea785
Diego Ruiz <dr1116uiz@gmail.com>
2025-12-09 01:07:43 -0600

    Updated install script - added error checking for zscroll and pulseaudio-control - config directories are made before cp is used - added more details to README.md

--- .scripts/install-dotfiles.sh
+++ .scripts/install-dotfiles.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "Downloading dotfiles..."
-cd $HOME/Downloads
-git clone https://github.com/sanicsquirtle420/dotfiles.git
-
 if [! -d $HOME/.scripts] ; then
 	mkdir $HOME/.scripts
 fi
@@ -12,7 +8,21 @@ if [! -d $HOME/.themes] ; then
 	mkdir $HOME/.themes
 fi
 
+echo "Creating config directories..."
+mkdir -p $HOME/.config
+mkdir -p $HOME/.config/cava
+mkdir -p $HOME/.config/dunst
+mkdir -p $HOME/.config/git
+mkdir -p $HOME/.config/i3
+mkdir -p $HOME/.config/kitty
+mkdir -p $HOME/.config/neofetch
+mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config/picom
+mkdir -p $HOME/.config/polybar
+mkdir -p $HOME/.config/rofi
+
 echo "Installing themes..."
+cd $HOME/Downloads
 cp -r dotfiles/.config/cava/* $HOME/.config/cava/
 cp -r dotfiles/.config/dunst/* $HOME/.config/dunst/
 cp -r dotfiles/.config/git/* $HOME/.config/git/
@@ -35,15 +45,26 @@ echo "Downloading zscroll"
 git clone https://github.com/noctuid/zscroll.git
 cd zscroll
 sudo python3 setup.py install
-cd $HOME/Downloads
-echo "zscroll installed!"
+
+if [ $? -ne 0 ]; then
+	echo "zscroll install failed"
+	exit
+else
+	echo "zscroll installed!"
+fi
 
 echo "Downloading pulseaudio-control"
+cd $HOME/Downloads
 git clone https://github.com/marioortizmanero/polybar-pulseaudio-control.git
 sudo mv $HOME/Downloads/polybar-pulseaudio-control/pulseaudio-control /usr/local/bin/
 sudo chmod +x /usr/local/bin/pulseaudio-control
-cd $HOME/Downloads
-echo "pulseaudio-control installed!"
+
+if [ $? -ne 0 ]; then
+	echo "pulseaudio-control install failed"
+	exit
+else
+	echo "pulseaudio-control installed!"
+fi
 
 THEME="aqua"
 $HOME/.scripts/change-to-$THEME.sh
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # SanicSquirtle420's dotfiles
-- OS: Arch Linux         
+- OS: [Arch Linux](https://archlinux.org)         
 - WM: i3
 
 ## Pictures
@@ -8,7 +8,7 @@
 Picture Taken: Dec 27, 2024
 
 ## NOTE: Themes
-Visit my [website](https://sanicsquirtle420.github.io/#/dotfiles) to see my themes! To see the images in their native resolution go to my [Tumblr](https://tumblr.com/blog/sanicsquirtle428) page
+Visit my [Github Pages](https://sanicsquirtle420.github.io/#/dotfiles) to see my themes!
 
 ## How to use my dotfiles
 I have created an install script `install-dotfiles.sh` to take care of the setup process. Before using the setup script download the following dependecies. You don't have to worry about the `pulseaudio-control` or `zscroll` setup because that is included in the install script. 
@@ -20,7 +20,9 @@ i3 playerctl rofi picom nitrogen lxappearance pulseaudio pavucontrol polybar kit
 
 ## To use the script
 ```bash
-cd Downloads/dotfiles/.scripts
+cd Downloads/
+git clone https://github.com/sanicsquirtle420/dotfiles.git
+cd dotfiles/
 chmod +x install-dotfiles.sh
 ./install-dotfiles.sh
 ```
@@ -30,9 +32,3 @@ chmod +x install-dotfiles.sh
 - [AurulentSansMono Nerd Font](https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/AurulentSansMono.zip)
 - [Noto Sans JP](https://fonts.google.com/noto/specimen/Noto+Sans+JP) (for Japanese Symbols)
 - [Noto Sans KR](https://fonts.google.com/noto/specimen/Noto+Sans+KR) (for Korean Symbols)
-
-### Applications Used For Ricing:
-```bash
-cmatrix ranger htop neofetch
-``` 
-[cava](https://github.com/karlstav/cava)
